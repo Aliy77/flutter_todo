a702f0192bff87740500566300bb7bd8ea8e31f9
Aliy77 <paranuk4477@gmail.com>
2023-05-25 16:12:50 +0300

    showing real weather

--- lib/pages/weather.dart
+++ lib/pages/weather.dart
@@ -6,6 +6,8 @@ import 'package:flutter_todo/main.dart';
 import 'package:flutter_todo/pages/home.dart';
 import 'package:flutter_todo/pages/main_screen.dart';
 import 'package:flutter_todo/widgets/time.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Weather extends StatelessWidget {
   const Weather({super.key});
@@ -18,6 +20,28 @@ class Weather extends StatelessWidget {
     
     double res_width = MediaQuery.of(context).size.width;
     double res_height = MediaQuery.of(context).size.height;
+  
+    Future<Map<String, dynamic>> _getWeatherData() async {
+      final String url = 'https://api.weather.yandex.ru/v2/forecast';
+
+      final response = await http.get(Uri.parse(url), headers: {
+        'X-Yandex-API-Key': '3092ad91-133b-4de7-9c87-d9d8304b7b3a',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      });
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(utf8.decode(response.bodyBytes));
+        return {
+          'temperature': data['fact']['temp'],
+          'description': data['fact']['condition'],
+          'cityName': data['geo_object']['locality']['name']
+        };
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -34,243 +58,63 @@ class Weather extends StatelessWidget {
           },
         ),
       ),
-      bottomNavigationBar: GestureDetector(
-        onTap: () {
-          showModalBottomSheet(
-              context: context,
-              elevation: 0,
-              barrierColor: Colors.black.withAlpha(1),
-              backgroundColor: Colors.transparent,
-              builder: (builder) {
-                return Container(
-                  height: 600.0,
-                  child: Container(
-                      decoration: const BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(50.0),
-                              topRight: Radius.circular(50.0))),
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 20, right: 20),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: res_height * 0.04,
-                            ),
-                            Row(
-                              children: [
-                                Image.asset(
-                                  'assets/images/clock.png',
-                                  width: 25,
-                                ),
-                                SizedBox(
-                                  width: res_width * 0.04,
-                                ),
-                                Text(
-                                  "Прогноз на сегодня",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w600),
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: res_height * 0.04,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Row(
-                                  children: [
-                                    CircleAvatar(
-                                      backgroundColor:
-                                          Color.fromARGB(255, 59, 114, 215).withOpacity(0.1),
-                                      radius: 25,
-                                      child: Image.asset(
-                                        'assets/images/rainy.png',
-                                        width: 25,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      "Дождь",
-                                      style: TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    CircleAvatar(
-                                      backgroundColor:
-                                          Color(0xffff5580).withOpacity(0.1),
-                                      radius: 25,
-                                      child: Image.asset(
-                                        'assets/images/wind.png',
-                                        width: 25,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 5,
-                                    ),
-                                    Text(
-                                      "Вететр\n1 м/с",
-                                      style: TextStyle(
-                                          fontSize: 17,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                            SizedBox(
-                              height: res_height * 0.04,
-                            ),
-                            Expanded(
-                              child: ListView.builder(
-                                  itemCount: data.length,
-                                  itemBuilder: (context, index) {
-                                    return Column(
-                                      children: [
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        TimeWidget(
-                                          data: data[index],
-                                          active: index == 1,
-                                        ),
-                                        SizedBox(
-                                          height: 5,
-                                        ),
-                                        Divider(color: Colors.grey)
-                                      ],
-                                    );
-                                  }),
-                            )
-                          ],
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: _getWeatherData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final temperature = snapshot.data!['temperature'];
+            final description = snapshot.data!['description'];
+            final cityName = snapshot.data!['cityName'];
+
+            return Container(
+              height: res_height * 1,
+              color: Color.fromARGB(255, 59, 119, 215),
+              child: Stack(
+                children: [
+                  //...
+                  Positioned(
+                    left: 20,
+                    top: res_height * 0.05,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'СР, 17 МАЯ',
+                          style: TextStyle(fontSize: 25, color: Colors.white),
                         ),
-                      )),
-                );
-              }).whenComplete(() {
-          });
-        },
-        child: Container(
-          //add ClipRRect widget for Round Corner
-          color: Color.fromARGB(255, 59, 121, 215),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topRight: Radius.circular(24),
-              topLeft: Radius.circular(24),
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-              ),
-              child: Padding(
-                padding:
-                    EdgeInsets.only(left: 13, right: 13, top: 13, bottom: 26),
-                child: ListTile(
-                  leading: SizedBox(
-                      height: 30.0,
-                      width: 30.0,
-                      child: Image.asset('assets/images/clock.png')),
-                  title: Text(
-                    "Прогноз на сегодня",
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                        Text(
+                          '$temperature°C',
+                          style: TextStyle(fontSize: 50, color: Colors.white),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          description,
+                          style: TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      ],
+                    ),
                   ),
-                  trailing: SizedBox(
-                      height: 30.0,
-                      width: 30.0,
-                      child: Image.asset('assets/images/calendar.png')),
-                ),
-              ),
-            ),
-          ),
-        ),
-      ),
-      body: Container(
-        height: res_height * 1,
-        color: Color.fromARGB(255, 59, 119, 215),
-        child: Stack(
-          children: [
-            AnimatedContainer(
-              duration: Duration(milliseconds: 600),
-              height: showdate ? res_height * 0.15 : res_height * 0.125,
-              width: double.infinity,
-            ),
-            Transform.rotate(
-              angle: math.pi / 4,
-              child: Container(
-                height: res_height * 1,
-                child: ParallaxRain(
-                  key: parallaxOne,
-                  dropColors: 
-                  [
-                    Colors.white,
-                  ],
-                  trail: true,
-                ),
-              ),
-            ),
-            Positioned(
-              right: 0,
-              top: res_height * 0.175,
-              child: Image.asset(
-                'assets/images/girl.png',
-                height: res_height * 0.5,
-              ),
-            ),
-            Positioned(
-              left: 20,
-              top: res_height * 0.05,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    'СР, 17 МАЯ',
-                    style: TextStyle(fontSize: 25, color: Colors.white),
-                  ),Text(
-                    '14°',
-                    style: TextStyle(fontSize: 50, color: Colors.white),
+                  Positioned(
+                    bottom: res_height * 0.02,
+                    left: 20,
+                    child: Text(
+                      cityName,
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Icon(
-                    Icons.cloudy_snowing,
-                    color: Colors.white,
-                    size: 40,
-                  )
                 ],
               ),
-            ),
-            Positioned(
-              bottom: res_height * 0.02,
-              left: 20,
-              child: Text(
-                'Майкоп',
-                style: TextStyle(fontSize: 40, color: Colors.white),
-              ),
-            ),
-          ],
-        ),
+            );
+          
+          } else if (snapshot.hasError) {
+            return Center(child: Text('${snapshot.error}'));
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
       ),
     );
   }
 }
-
-var data = [
-  {'time': '09:00', 'temp': '12°'},
-  {'time': '10:00', 'temp': '14°'},
-  {'time': '11:00', 'temp': '17°'},
-  {'time': '12:00', 'temp': '19°'},
-  {'time': '13:00', 'temp': '22°'},
-  {'time': '14:00', 'temp': '22°'},
-  {'time': '15:00', 'temp': '21°'},
-  {'time': '16:00', 'temp': '20°'},
-  {'time': '17:00', 'temp': '20°'},
-  {'time': '18:00', 'temp': '18°'},
-];
